c063cea984c9bdc2a051f89b1cbf52ad2753d89d
Koen Castermans <castermanskoen@msn.com>
2017-04-15 16:22:24 +0200

    - Set up first event listeners - Reading input data - Initialization function - Income and Expense function constructors

--- app.js
+++ app.js
@@ -2,20 +2,103 @@
 // Data module
 var budgetController = (function () {
 
-    // Some code
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            allExpenses: [],
+            allIncomes: [],
+        },
+        totals: {
+            totalExpenses: 0,
+        },
+    };
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem;
+
+            
+        },
+    };
 
 })();
 
 // UI module
 var UIController = (function () {
 
-    // Some code
+    // Set all used DOM string in a object
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            };
+        },
+
+        getDomStrings: function () {
+            return DOMstrings;
+        },
+    };
 
 })();
 
-// Main controller => connects the two other modules
+// Main app controller => connects the two other modules
 var controller = (function (budgetCtrl, UICtrl) {
 
-    
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDomStrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
+    var ctrlAddItem = function () {
+        // 1. Get the field input data
+        var input = UICtrl.getInput();
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
+    };
+
+    return {
+        init: function () {
+            console.log('Application has started.');
+            setupEventListeners();
+        },
+    };
 
 })(budgetController, UIController);
+
+controller.init(); // initialise the app
